0b4e06044fc7a13aade13469f74636ab3a10db5c
vladoje <vladorakic07@gmail.com>
2025-06-13 10:39:30 +0200

    fix url-a; host render.com

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/')
+    fetch('https://milionski-projekat.onrender.com/')
       .then(res => res.text())
       .then(data => setMessage(data));
   }, []);
