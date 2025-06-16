f35fd496eb1bda33850560a0955361421d17521e
vladoje <vladorakic07@gmail.com>
2025-06-16 10:50:22 +0200

    test greske

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+console.log(process.env.MONGO_URI);
 
 // Povezivanje sa MongoDB
 mongoose.connect(process.env.MONGO_URI, {
