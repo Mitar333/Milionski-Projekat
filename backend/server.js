87c4d02912b2b7da0f55ffb7c89ab18c67b64df9
vladoje <vladorakic07@gmail.com>
2025-07-06 11:19:20 +0200

    greska

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 //const User = require('./models/userSchema');
-const userRoutes = require('./routes/userRoutes');
+//const userRoutes = require('./routes/userRoutes');
+
 
 //const ApointmentRoutes = require('./routes/ApointmentRoutes');
 //const Apointment = require('./models/Apointment');
@@ -15,22 +16,40 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
+
+
+
+
+// Definisanje ruta
+//app.use("/api/users", userRoutes);
 // app.use("/api/appointments", ApointmentRoutes);
+
+
+
+// Detektuje ako si ukucao pogresan url
+app.all("*",(req,res,next)=>{
+  const err=new Error(`Ruta ${req.originalUrl} nije pronađena`);
+  err.status="fail";
+  err.statusCode=404;
+  next(err)
+})
+
+app.use((err,req,res,next)=>{
+  err.statusCode= err.statusCode || 500;
+  err.status=err.status||"error";
+  res.status(err.statusCode).json({
+    status:err.status,
+    message:err.message
+  })
+})
+
+// app.use()
+
+
 
 // Povezivanje sa MongoDB
 mongoose.connect(process.env.MONGO_URI, {}).then(() => console.log('MongoDB konektovan'))
 .catch(err => console.error('Greška pri konekciji:', err));
-
-//povezivanje sa rutama
-// app.use("/api",userRoutes)
-// app.use("/api",ApointmentRoutes)
-
-// Error handling middleware (optional)
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server radi na http://localhost:${PORT}`);
