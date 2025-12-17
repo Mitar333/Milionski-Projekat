7cb6c3e812634d0e4a9a76d393f486a533d2e817
vladoje <vladorakic07@gmail.com>
2025-12-17 13:56:55 +0100

    not found uljepsan

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <Header label="Pogresan url" />
+      <div className="my-48 flex">
+        <Link
+          to="/"
+          className="mx-auto w-72 text-center bg-indigo-500 text-white py-2.5 rounded-xl font-medium text-sm"
+        >
+          Nazad na pocetak
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default NotFound;
